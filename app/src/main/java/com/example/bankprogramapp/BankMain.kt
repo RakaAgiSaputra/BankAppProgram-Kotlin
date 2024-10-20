fe80684c452de44005d3a0e5971cee88dd103f30
Raka Agi <Raka_Agi_Saputra@teknokrat.ac.id>
2024-10-20 10:18:04 +0700

    agi: ubah nama user

--- app/src/main/java/com/example/bankprogramapp/BankMain.kt
+++ app/src/main/java/com/example/bankprogramapp/BankMain.kt
@@ -1,7 +1,7 @@
 package com.example.bankprogramapp
 
 fun main(){
-    val dennisUser = BankAccount("Denis Panjagal",50.000)
+    val dennisUser = BankAccount("Deni Panjagal",50.000)
     dennisUser.deposit(10.000)
     dennisUser.deposit(10.000)
     dennisUser.withdraw(5.000)
